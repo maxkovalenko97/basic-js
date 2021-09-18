13c2811b41694a4ff61559fa55d32d7ac7077546
Max Kovalenko <tutmrmaxi@gmail.com>
2021-09-18 23:04:46 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(strp, options) {
+  let arr = [];
+  let str = String(strp);
+  let separator = '+';
+  if(options.separator) separator = options.separator;
+  let additionSeparator = '|';
+  if(options.additionSeparator) additionSeparator = options.additionSeparator;
+
+  let repeatTimes = 0;
+  if(options.repeatTimes) repeatTimes = options.repeatTimes;
+  let additionRepeatTimes = 0;
+  if(options.additionRepeatTimes) additionRepeatTimes = options.additionRepeatTimes;
+
+  let addition;
+  if(options.addition === undefined) {
+    addition = '';
+  }
+  else addition = String(options.addition);
+
+  if(!additionRepeatTimes) { str = str + addition};
+  if(additionRepeatTimes >= 1) {
+    addition = addition + additionSeparator;
+    addition = addition.repeat(additionRepeatTimes);
+    addition = addition.slice(0 , (addition.length - additionSeparator.length))
+    str = str + addition;
+  }
+
+  if(repeatTimes == 1 || repeatTimes == 0) {console.log(str); return str}
+  else {
+      for(let i=0; i<repeatTimes; i++) {
+          if(i == repeatTimes-1) {arr.push(str)}
+          else {arr.push(str + separator)}
+      }    
+  }
+  console.log(arr.join(''));
+  return(arr.join(''))
 }
