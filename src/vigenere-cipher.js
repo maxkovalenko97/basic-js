7e4106aec0b5f021218b8d26f0b90c8b516cc840
Max Kovalenko <tutmrmaxi@gmail.com>
2022-05-02 23:09:05 +0300

    feat: fix vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,18 +20,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    if(str == undefined || key == undefined) {
-      throw new Error('Incorrect arguments!');
-    }
+
+  constructor(direction) {
+    if(direction == false) this.direction = 'reverse';
+    else this.direction = 'direct';
   }
-  decrypt() { //str, key
-    throw new NotImplementedError('Not implemented')
-    if(str == undefined || key == undefined) {
-      throw new Error('Incorrect arguments!');
-    }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    return  this.encode(message, key, 'encrypt');
   }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!')
+    return this.encode(message, key);
+  }
+
+  encode(message, key, method) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let letters = message.match(/[a-z]/gi);
+    if (!letters) return message;
+    
+    let changedKey = key.repeat(Math.ceil(letters.length/key.length)).split('').map(char =>alphabet.indexOf(char.toUpperCase()));
+    let i = 0;
+    let result = message.split('').map(char =>{
+      if (/[a-z]/i.test(char)) {
+        let changedLetter = alphabet.indexOf(char.toUpperCase());
+        let letterCode = method == 'encrypt' ? ((changedLetter + changedKey[i++]) % 26) : ((changedLetter - changedKey[i++] + 26) % 26);
+        return alphabet[letterCode];
+        }
+      else return char;
+      });
+      
+    return this.direction == 'direct' ? result.join('') : result.reverse().join('');;
+  }
+
 }
 
 module.exports = {
